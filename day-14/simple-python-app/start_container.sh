84f9a61354d2dcc94487486b69189dffb86ab4bc
Python6042 <53249925+Python6042@users.noreply.github.com>
2023-07-21 18:23:44 +0530

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
-
 # Pull the Docker image from Docker Hub
-echo
-
+docker pull 254879/simple-python-flask-app
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:500 254879/simple-python-flask-app
