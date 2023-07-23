fb3be07d2ab234f9ab2f19004cc0e710dea02e44
Python6042 <53249925+Python6042@users.noreply.github.com>
2023-07-23 12:22:13 +0530

    Update stop_containers.sh

--- stop_containers.sh
+++ stop_containers.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 # Find the running container (if any)
-Containers_id= `docker -ps | awk -F " " '{print $1}'`
+Containers_id= `docker ps | awk -F " " '{print $1}' | sed 1d`
 #remove the running containers
-rm -rf $Containers_id
+docker rm -rf $Containers_id
+#docker rm -f $(docker ps | awk -F " " '{print $1}' | sed 1d)
